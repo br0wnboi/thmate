0b172482298218411022d7460fb805f622ed54c3
AYAN-AMBESH <ambesh12k@gmail.com>
2021-08-27 15:24:56 +0530

    Added CLI

--- v1.sh
+++ v1.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/bash
-banner -l thmate
+USERNAME=$1
+banner  thmate
 TOTALUSERS=$(curl -s https://tryhackme.com/api/site-stats | jq '.totalUsers')
 echo "Total TryHackMe Users: $TOTALUSERS"
-read -p "Enter your TryHackMe Username: " USERNAME;
 # Welcome User:
 echo "Hi $USERNAME";
 # Grab UserDetails
-RANK=$(curl -s https://tryhackme.com/api/user/rank/$USERNAME | jq '.userRank') 
-BADGE=$(curl -s https://tryhackme.com/api/badges/get/$USERNAME | jq '.[] | .name' | wc -l)
-DONEROOMS=$(curl -s https://tryhackme.com/api/no-completed-rooms-public/$USERNAME)
+RANK=$(curl -s https://tryhackme.com/api/user/rank/$USERNAME | grep -o '[0-9]*')
+BADGE=$(curl -s https://tryhackme.com/api/badges/get/$USERNAME|grep -o '[0-9]*' | wc -l)
+DONEROOMS=$(curl -s https://tryhackme.com/api/no-completed-rooms-public/$USERNAME | grep -o '[0-9]*')
 echo "Rank: $RANK"
 echo "Completed Rooms: $DONEROOMS"
 echo "Badges Earned: $BADGE"
